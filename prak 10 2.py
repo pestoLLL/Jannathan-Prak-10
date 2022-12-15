b7ace68063d2aafcb4311b2350593dfd2568aeba
pestoLLL <113655363+pestoLLL@users.noreply.github.com>
2022-12-15 13:13:45 +0700

    Add files via upload

---
+++ prak 10 2.py
@@ -0,0 +1,13 @@
+def bubbleSort(a,b=0):
+ n = len(a)
+ if b<n:
+    for j in range(0, n-b-1):
+        if a[j] > a[j+1] :
+            a[j], a[j+1] = a[j+1], a[j]
+            b+=1
+            bubbleSort(a,b)
+ return a
+data= [87,56, 34, 23,89,15,2,200,28,31, 2]
+print(data)
+urut=bubbleSort(data)
+print(urut)
